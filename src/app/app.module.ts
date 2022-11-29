b7775239cfec95d440a54b64cf545d8b2debf6fd
athulajayghosh <53214137+athulajayghosh@users.noreply.github.com>
2022-11-29 09:21:14 +0530

    dev push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewallComponent } from './viewall/viewall.component';
+
+const myRoute: Routes=[
+  {path:"",
+  component:ViewallComponent
+  }]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewallComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
